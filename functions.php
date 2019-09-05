0b450774718f5cf5f9882cc61d288eff3da34102
gonzalo torres <conchax@gmail.com>
2019-09-05 04:27:24 -0500

    agregando

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function lapizzeria_styles(){
+//registrar estilos
+  wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '5.0.0');
+  wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array('normalize'), '4.7.0');
+  wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
+//LLamar estilos
+  wp_enqueue_style('normalize');
+  wp_enqueue_style('fontawesome');
+  wp_enqueue_style('style');
+}
+add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+function lapizzeria_menus(){
+  register_nav_menus(array(
+    'header-menu' => __('Header Menu', 'lapizzeria'),
+    'social-menu' => __('Social Menu', 'lapizzeria')
+  ));
+}
+
+add_action('init', 'lapizzeria_menus');
